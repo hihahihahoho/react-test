ba1b4bef66ffd46c4f4f39e6b7d1fdc77b42fda8
tungnt2 <tungnt2@vnpay.vn>
2021-12-31 17:33:40 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@
 import Header from './components/Header'
 import Counter from './components/Counter'
 import CounterHooks from './components/CounterHooks'
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       </div>
       <Counter initialCount={1} />
       <CounterHooks initialCount={0} />
+      <DatePicker></DatePicker>
+      <DatePicker></DatePicker>
     </>
   );
 }
